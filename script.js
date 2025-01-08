0cc7a6f7bbee989d96ad8e54cb9033f60a885a63
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-01-08 13:02:33 -0800

    question 1

---
+++ script.js
@@ -0,0 +1,12 @@
+function Power(num , num2){
+  let reslt  = 1
+for(let i =1; i<=num2 ; i++){
+  console.log(num2)
+reslt = reslt *num
+}
+return  reslt
+}
+
+
+console.log(Power(2, 3)) //Multiple it 
+
