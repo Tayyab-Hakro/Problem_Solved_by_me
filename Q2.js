594576abda2900f8aedada7988500e8a32272eee
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-01-08 19:22:50 -0800

    Calculate Average of the numbers

---
+++ Q2.js
@@ -0,0 +1,13 @@
+function CalculateAverg (arr){
+    let =total = 0
+for(let i =0 ; i<arr.length ; i++){
+    total +=arr[i]
+}
+total = total /arr.length
+return total
+}
+
+
+
+
+console.log(CalculateAverg([1,2,4]))
