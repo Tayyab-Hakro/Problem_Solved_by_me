45dfde61a74519cfec0da8f92554a4dfc3c94df0
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-01-14 10:20:57 -0800

    mERGE to Array

---
+++ Q5.js
@@ -0,0 +1,20 @@
+//Write a function that will Merge two array
+
+function MergeArr(arr1, arr2) {
+   let  Arr3 =[]
+for(let i =0 ; i<arr1.length ; i++){
+    Arr3.push(arr1[i])
+
+}
+for(let j =0 ; j<arr2.length ; j++){
+    Arr3.push(arr2[j])
+}
+return Arr3
+}
+
+// Example usage
+let result = MergeArr([1, 2, 3], [4, 5, 6]);
+console.log(result); // Output: [1, 2, 3, 4, 5, 6]
+
+
+
