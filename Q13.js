f1e0c922c43da9c7f206eb70e457bef1e89fadca
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-01-26 11:09:16 -0800

    Q13 //Make Sider When click on button it show sidebar

---
+++ Q13.js
@@ -0,0 +1,12 @@
+//Make Sider When click on button it show sidebar
+
+
+function HandleHidden(){
+let show =   document.querySelector(".uls")
+if(show.style.display === "none"){
+    show.style.display ="block"
+}else{
+    show.style.display ="none"
+    
+}
+}
