095ddce0252e793e74ebb7d66d1194915d07e571
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-01-25 23:19:05 -0800

    Fint the value in array = 30

---
+++ Q11.js
@@ -0,0 +1,12 @@
+//Fint the value in array = 30
+
+const arr = [34,64,84, 30,12,54]
+const find = 30
+
+for (let i =0 ;i<arr.length ;i++){
+if(arr[i] === find){
+    console.log(arr[i])
+    break
+}
+}
+
