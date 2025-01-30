cecedb21cd6f493dd0318bbeb15ee1edd54a93a4
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-01-29 22:10:14 -0800

    Write a function that takes two strings as input and checks whether they are anagrams (contain the same characters in any order

---
+++ Q16.js
@@ -0,0 +1,12 @@
+function areAnagrams(str1, str2) {
+    // If lengths are different, they can't be anagrams
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    // Sort both strings and compare
+    return str1.split('').sort().join('') === str2.split('').sort().join('');
+}
+
+console.log(areAnagrams("listen", "silent")); // Output: true
+console.log(areAnagrams("hello", "world"));   // Output: false
