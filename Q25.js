6b2357cf3879256b2d5d4ab434414b2a230b2268
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-02-03 15:42:42 -0800

    Question 25 Solved

---
+++ Q25.js
@@ -0,0 +1,12 @@
+// Implement a function to reverse the words in a 
+// given sentence
+//  (e.g., "Hello World" → "World Hello").
+
+
+function ReverseSentence (Str){
+Str = Str.split(" ")
+let LastStr = Str[1] + Str[0]
+return LastStr
+}
+
+console.log(ReverseSentence("Hello World"))
