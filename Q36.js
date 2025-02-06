06286cc062dda5e943c67b6174239220b0e73764
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-02-05 18:03:28 -0800

    Question 36 Completed

---
+++ Q36.js
@@ -0,0 +1,7 @@
+//Create a function that will check if string is starting with specfic Character
+
+function SpecficCharater (str ,char){
+return str.toLowerCase().startsWith(char)
+}
+
+console.log(SpecficCharater("Go to gome " ,"g"))
