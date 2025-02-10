9ead14e4fe7c3896aa43256bcc1f3b0f3c96e76e
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-02-09 20:06:56 -0800

    solved

---
+++ Oops_Questions/Q8.js
@@ -0,0 +1,19 @@
+class Car {
+    constructor() {
+        this._speed = 0; // Private property with default value
+    }
+
+    // Setter method to set speed
+    set speed(value) {
+        this._speed = value; 
+    }
+
+    // Getter method to get speed
+    get speed() {
+        return this._speed;
+    }
+}
+
+const myCar = new Car();
+myCar.speed = 180;  // Setting speed
+console.log(myCar.speed); // Output: 80
