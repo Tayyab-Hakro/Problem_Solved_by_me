f9f8412b472b916ae255468b377bff708d5cb1a1
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-02-13 08:12:48 -0800

    Question solved

---
+++ Q53.js
@@ -0,0 +1,18 @@
+// You are given a string s. The score of a string is defined as the sum of the
+//  absolute difference between the ASCII values of adjacent characters.
+// Return the score of s.
+// // Input: s = "hello"
+// // Output: 13
+// // The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 
+// // 'o' = 111. So, the score of s would be 
+// // |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+// charCodeAt
+var scoreOfString = function(s) {
+    let score = 0
+    for(let i = 0 ; i <s.length-1 ; i++){
+score += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1) )
+console.log(score)    
+}
+ return score
+}
+console.log(scoreOfString("hello"))
