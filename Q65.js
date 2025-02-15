42b9f1bd1b096931366925780c55403c7363043f
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-02-15 10:44:17 -0800

    Question Solved

---
+++ Q65.js
@@ -0,0 +1,11 @@
+//Find the explooye whose worked more then target
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let count  = 0
+      for(let i = 0; i<=hours.length ; i++){
+       if(hours[i] >= target){
+ count++
+}
+  };
+  return count
+}
+  console.log(numberOfEmployeesWhoMetTarget([0,1,2,3,4],2))
